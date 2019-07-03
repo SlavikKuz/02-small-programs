05f70399d627965298b508b3fbc09904eb2cbfa3
Slavik <slavik.kuz.mail@gmail.com>
2019-07-03 18:26:49 +0200

    builders

---
+++ WebScrapper/Builders/ScrapeCriteriaBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebScrapper.Data;
+
+namespace WebScrapper.Builders
+{
+    class ScrapeCriteriaBuilder
+    {
+        private string _data;
+        private string _regex;
+        private RegexOptions _regexOption;
+        private List<ScrapeCriteriaParts> _parts;
+
+        public ScrapeCriteriaBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _data = string.Empty;
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+            _parts = new List<ScrapeCriteriaParts>();
+        }
+
+        public ScrapeCriteriaBuilder WithData(string data)
+        {
+            _data = data;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegexOptions(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithPart(ScrapeCriteriaParts scrapeCriteriaParts)
+        {
+            _parts.Add(scrapeCriteriaParts);
+            return this;
+        }
+
+        public ScrapeCriteria Build()
+        {
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteria();
+            scrapeCriteria.Data = _data;
+            scrapeCriteria.Regex = _regex;
+            scrapeCriteria.RegexOption = _regexOption;
+            scrapeCriteria.Parts = _parts;
+            return scrapeCriteria;
+        }
+    }
+}
---
+++ WebScrapper/Builders/ScrapeCriteriaPartBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebScrapper.Data;
+
+namespace WebScrapper.Builders
+{
+    class ScrapeCriteriaPartBuilder
+    {
+        private string _regex;
+        private RegexOptions _regexOption;
+
+        public ScrapeCriteriaPartBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOptions(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaParts Build()
+        {
+            ScrapeCriteriaParts scrapeCriteriaParts = new ScrapeCriteriaParts();
+            scrapeCriteriaParts.Regex = _regex;
+            scrapeCriteriaParts.RegexOption = _regexOption;
+            return this;
+        }
+    }
+}
