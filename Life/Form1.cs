eb684bae143812000523299cabdcd3c88ed540eb
Slavik <slavik.kuz.mail@gmail.com>
2019-07-10 08:12:18 +0200

    The Game of Life

---
+++ Life/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Life
+{
+    public partial class Form1 : Form
+    {
+        int box_w = 20;
+        int box_h = 20;
+        int w, h;
+        Label[,] lab;
+        Life life;
+        Color col_empty = Color.White;
+        Color col_live = Color.Blue;
+        Color col_born = Color.Green;
+        Color col_dead = Color.Red;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Controls.Clear();
+            InitLabels();
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)//rebuild field when resized
+        {
+            panel1.Controls.Clear();
+            InitLabels();
+        }
+
+        private void InitLabels()//adding labels
+        {
+            w = panel1.Width / box_w;
+            h = panel1.Height / box_h;
+
+            life = new Life(w, h);
+            lab = new Label[w, h];
+
+            for (int i = 0; i < w; i++)
+                for (int j = 0; j < h; j++)
+                    AddLabel(i, j);
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)//mouse paint
+        {
+            int x = ((Label)sender).Location.X / box_w;
+            int y = ((Label)sender).Location.Y / box_h;
+            int color = life.SwitchField(x, y);
+            lab[x, y].BackColor = color == 1 ? col_live : col_empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)//fase 1
+        {
+            life.FaseOne();
+            RefreshField();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//fase 2
+        {
+            life.FaseTwo();
+            RefreshField();
+        }
+
+        private void RefreshField()//colorfill
+        {
+            for (int x = 0; x < w; x++)
+                for (int y = 0; y < h; y++)
+                    switch(life.ReadField(x,y))
+                    {
+                        case 0: lab[x, y].BackColor = col_empty; break;
+                        case 1: lab[x, y].BackColor = col_live; break;
+                        case 2: lab[x, y].BackColor = col_dead; break;
+                        case -1: lab[x, y].BackColor = col_born; break;
+                    }
+        }
+
+        private void button3_Click(object sender, EventArgs e)//proceed
+        {
+            life.FaseOne();
+            life.FaseTwo();
+            RefreshField();
+        }
+
+        private void button4_Click(object sender, EventArgs e)//timer
+        {
+            timer.Enabled = !timer.Enabled;
+            if (timer.Enabled)
+            {
+                button4.Text = "Timer is ON";
+                button4.ForeColor = Color.Red;
+            }
+            else
+            {
+                button4.Text = "Timer is OFF";
+                button4.ForeColor = Color.Black;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)//clear
+        {
+            panel1.Controls.Clear();
+            InitLabels();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)//timer logic performance
+        {
+            life.FaseOne();
+            life.FaseTwo();
+            RefreshField();
+        }
+
+        private void button5_Click(object sender, EventArgs e)//genrate cells button
+        {
+            life.Contamine();
+            RefreshField();
+        }
+
+        private void button6_Click(object sender, EventArgs e)//rules. Horrible!
+        {
+            MessageBox.Show("Hello! This is a classical Game of Life game invited by John Conway. " + 
+                Environment.NewLine +
+                Environment.NewLine +
+                "A living cell is blue. It survives only when surrounded by two other cells. " +
+                "A new cell is born if surrounded by three cells. Every cell dies when it is has less or more neighbours." +
+                 Environment.NewLine +
+                 Environment.NewLine +
+                 "Please, use the mouse to build any lifeform that you like! You can add more cells while the evolution is running :)" +
+                 Environment.NewLine +
+                 Environment.NewLine +
+                "Fase One - highlights the cells that are dying(red), newborn(green) and survived(blue)." +
+                 Environment.NewLine +
+                "Fase Two - finishes the turn with removing dead cells and establishing newborn and survived cells." +
+                 Environment.NewLine +
+                "Proceed fases - combines both fases in one step." +
+                 Environment.NewLine +
+                "Contaminate - randomly fills the field with live cells." +
+                 Environment.NewLine +
+                "Clear - clears the field. The field is resizable and every change of sizes clears the field." +
+                 Environment.NewLine +
+                "Timer - starts the Life game until stopped." +
+                 Environment.NewLine +
+                 Environment.NewLine +
+                "The game was programmed as an array of labels. The mathematical model is based on subtraction of fields with cells. " +
+                "The program calculates sums of cells that are to the right and down from a target cell and subtract them. " +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Have fun!", 
+                "Short description", MessageBoxButtons.OK);
+        }
+
+        private void AddLabel(int x, int y) // code for every label
+        {
+            lab[x, y] = new Label();
+            lab[x, y].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            lab[x, y].Location = new System.Drawing.Point(x*box_w, y*box_h);
+            lab[x, y].Size = new System.Drawing.Size(box_w + 1, box_h + 1);
+            lab[x, y].Parent = panel1;
+            lab[x, y].MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Life/Life.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    class Life
+    {
+        int[,] field; // 0 - empty, 1 - lifeform, -1 - newborn, 2 - dead
+        int[,] sum; // number og lifeforms to the right and down from the cell 
+        int width, height;
+        Random random;
+
+        public Life(int w, int h) //creates array of empty cells
+        {
+            this.width = w;
+            this.height = h;
+            field = new int [width, height];
+            sum = new int[width, height];
+
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    field[i, j] = 0; //every cell is unpopulated
+        }  
+
+        public int SwitchField (int x, int y)//empty cell becomes 1 and vice versa
+        {
+            field[x, y] = field[x, y] == 0 ? 1 : 0;
+            return field[x, y];
+        }
+
+        public int ReadField (int x, int y)//cell value becomes 0, if dead or out of borders
+        {
+            if (x < 0 || x >= width)
+                return 0;
+            if (y < 0 || y >= height)
+                return 0;
+            return field[x, y]; 
+        }
+
+        public int ReadSum(int x, int y)//sum of lifeforms
+        {
+            if (x >= width)
+                return 0;
+            if (y >= height)
+                return 0;
+            if (x < 0) x=0;
+            if (y < 0) y=0;// borders of field reached
+            return sum[x, y];
+        }
+
+        public void Contamine()
+        {
+            random = new Random();
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                {
+                    field[i, j] = random.Next(100) % 2;//generate 1 and 0
+                    if (i * j % 3 == 1 || i * j % 5 == 1) //remove some cells
+                        field[i, j] = 0;
+                }
+        }
+
+        //ordinary logic for surrounding cells
+        //public int Surround(int x, int y)
+        //{
+        //    int sum = 0;
+        //    for (int ax = -1; ax <= 1; ax++)
+        //        for (int ay = -1; ay <= 1; ay++)
+        //            if (ReadField(x + ax, y + ay) > 0)
+        //                sum++;
+        //    return sum;
+        //}
+
+        public int Surround2(int x, int y)//select middle cell in 3x3 and summs of fields
+        {
+            return ReadSum(x-1, y-1) -
+                ReadSum(x + 2, y-1) -
+                ReadSum(x-1, y + 2) +
+                ReadSum(x + 2, y + 2);
+        }
+
+        private void Prepare()//summs
+        {
+            for (int x = width - 1; x >= 0; x--)
+                for (int y = height - 1; y >= 0; y--)
+                {
+                    sum[x, y] = ReadField(x,y)+
+                        ReadSum(x + 1, y) + 
+                        ReadSum(x, y + 1) - 
+                        ReadSum(x + 1, y + 1);
+                }
+        }
+       
+        public void FaseOne() //mark newborn and dead
+        {
+            Prepare();
+            for (int i=0; i<width;i++)
+                for (int j=0; j<height; j++)
+                {
+                    int a = Surround2(i, j);
+                    if (field[i,j]==1)
+                    {
+                        if (a <= 2)
+                            field[i, j] = 2; //alone
+                        if (a >= 5)
+                            field[i, j] = 2; //overcrowded
+                    }
+                    else
+                    {
+                        if (a == 3)
+                            field[i, j] = -1; //newborn
+                    }
+                }
+        }
+
+        public void FaseTwo() //remove dead and place newborn
+        {
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                {
+                    if (field[i, j] == -1)
+                        field[i, j] = 1;
+                    else if (field[i,j]== 2)
+                        field[i, j] = 0;
+                }
+                   
+        }
+
+    }
+}
