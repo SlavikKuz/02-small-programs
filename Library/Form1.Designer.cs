18ce69d8dcabe41689be4bbc125f6f3fe6ab74bf
Slavik <slavik.kuz.mail@gmail.com>
2019-07-10 21:15:39 +0200

    library

---
+++ Library/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿using System.Windows.Forms;
+
+namespace CourseTask1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelAuthor = new System.Windows.Forms.Label();
+            this.labelBook = new System.Windows.Forms.Label();
+            this.authorTextBox = new System.Windows.Forms.TextBox();
+            this.bookTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.NameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BookColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.YearColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PublisherColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EditionColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.DelTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yearTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.publisherTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.editionComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelAuthor
+            // 
+            this.labelAuthor.AutoSize = true;
+            this.labelAuthor.Location = new System.Drawing.Point(895, 257);
+            this.labelAuthor.Name = "labelAuthor";
+            this.labelAuthor.Size = new System.Drawing.Size(57, 20);
+            this.labelAuthor.TabIndex = 0;
+            this.labelAuthor.Text = "Author";
+            // 
+            // labelBook
+            // 
+            this.labelBook.AutoSize = true;
+            this.labelBook.Location = new System.Drawing.Point(895, 313);
+            this.labelBook.Name = "labelBook";
+            this.labelBook.Size = new System.Drawing.Size(46, 20);
+            this.labelBook.TabIndex = 1;
+            this.labelBook.Text = "Book";
+            this.labelBook.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // authorTextBox
+            // 
+            this.authorTextBox.Location = new System.Drawing.Point(899, 280);
+            this.authorTextBox.Name = "authorTextBox";
+            this.authorTextBox.Size = new System.Drawing.Size(334, 26);
+            this.authorTextBox.TabIndex = 2;
+            this.authorTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.authorTextBox_KeyPress);
+            // 
+            // bookTextBox
+            // 
+            this.bookTextBox.Location = new System.Drawing.Point(899, 336);
+            this.bookTextBox.Name = "bookTextBox";
+            this.bookTextBox.Size = new System.Drawing.Size(334, 26);
+            this.bookTextBox.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(899, 551);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(334, 60);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add new";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(953, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameColumn,
+            this.BookColumn,
+            this.YearColumn,
+            this.PublisherColumn,
+            this.EditionColumn});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(860, 690);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.Text = "Name";
+            this.NameColumn.Width = 110;
+            // 
+            // BookColumn
+            // 
+            this.BookColumn.Text = "Book";
+            this.BookColumn.Width = 260;
+            // 
+            // YearColumn
+            // 
+            this.YearColumn.Text = "Year";
+            this.YearColumn.Width = 40;
+            // 
+            // PublisherColumn
+            // 
+            this.PublisherColumn.Text = "Publisher";
+            this.PublisherColumn.Width = 80;
+            // 
+            // EditionColumn
+            // 
+            this.EditionColumn.Text = "Edition";
+            this.EditionColumn.Width = 80;
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Location = new System.Drawing.Point(1158, 641);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(75, 58);
+            this.buttonDel.TabIndex = 8;
+            this.buttonDel.Text = "Delete";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.buttonRec_Click);
+            // 
+            // DelTextBox
+            // 
+            this.DelTextBox.Location = new System.Drawing.Point(899, 664);
+            this.DelTextBox.Name = "DelTextBox";
+            this.DelTextBox.Size = new System.Drawing.Size(253, 26);
+            this.DelTextBox.TabIndex = 9;
+            this.DelTextBox.TextChanged += new System.EventHandler(this.DelTextBox_TextChanged);
+            this.DelTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DelTextBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(901, 641);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Please enter filter lettter:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(919, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Performed by Viacheslav Kuz, 13322";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(899, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Year";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // yearTextBox
+            // 
+            this.yearTextBox.Location = new System.Drawing.Point(899, 393);
+            this.yearTextBox.Name = "yearTextBox";
+            this.yearTextBox.Size = new System.Drawing.Size(334, 26);
+            this.yearTextBox.TabIndex = 13;
+            this.yearTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.yearTextBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(899, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Publisher";
+            // 
+            // publisherTextBox
+            // 
+            this.publisherTextBox.Location = new System.Drawing.Point(899, 450);
+            this.publisherTextBox.Name = "publisherTextBox";
+            this.publisherTextBox.Size = new System.Drawing.Size(334, 26);
+            this.publisherTextBox.TabIndex = 15;
+            this.publisherTextBox.TextChanged += new System.EventHandler(this.publisherTextBox_TextChanged);
+            this.publisherTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.publisherTextBox_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(899, 483);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Edition";
+            // 
+            // editionComboBox
+            // 
+            this.editionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.editionComboBox.FormattingEnabled = true;
+            this.editionComboBox.Items.AddRange(new object[] {
+            "Paper",
+            "E-book"});
+            this.editionComboBox.Location = new System.Drawing.Point(899, 506);
+            this.editionComboBox.Name = "editionComboBox";
+            this.editionComboBox.Size = new System.Drawing.Size(334, 28);
+            this.editionComboBox.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 714);
+            this.Controls.Add(this.editionComboBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.publisherTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.yearTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DelTextBox);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.bookTextBox);
+            this.Controls.Add(this.authorTextBox);
+            this.Controls.Add(this.labelBook);
+            this.Controls.Add(this.labelAuthor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1270, 770);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Course Task 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelAuthor;
+        private System.Windows.Forms.Label labelBook;
+        private System.Windows.Forms.TextBox authorTextBox;
+        private System.Windows.Forms.TextBox bookTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.ColumnHeader BookColumn;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.TextBox DelTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox yearTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox publisherTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox editionComboBox;
+        private System.Windows.Forms.ColumnHeader YearColumn;
+        private System.Windows.Forms.ColumnHeader PublisherColumn;
+        private System.Windows.Forms.ColumnHeader EditionColumn;
+    }
+}
+
---
+++ Library/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CourseTask1
+{
+    public partial class Form1 : Form
+    {
+        public static class Globals
+        {
+            //public static String Dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName 
+            //                           + "\\Data",                        //project folder for testing
+            //                    File = Dir+"\\database.csv";
+
+            public static String Dir = Application.StartupPath + "\\Data", //folder where the program is running
+                File = Dir + "\\database.csv";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists(Globals.File))
+            {
+                int lineCount = File.ReadLines(Globals.File).Count(); // length of the array
+
+                string[] split = new string[5];
+                StreamReader read = new StreamReader(Globals.File); //the array to the screen
+                for (int i = 0; i < lineCount; i++)
+                {
+                    split = read.ReadLine().Split(';'); //break the line for output in the form
+                    ListViewItem lvi = new ListViewItem(split);
+                    listView1.Items.Add(lvi); //bring out one by one to the screen
+                }
+
+                read.Close();
+            }
+            else
+            {
+                MessageBox.Show("Database is empty!");
+                if (!Directory.Exists(Globals.Dir)) Directory.CreateDirectory(Globals.Dir);
+                File.Create(Globals.File).Close();
+            }
+        }
+
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(authorTextBox.Text)) // checking the author empty input
+            {
+
+                string fullLine = authorTextBox.Text // full line to write to file
+                                  + ";"
+                                  + bookTextBox.Text
+                                  + ";"
+                                  + yearTextBox.Text
+                                  + ";"
+                                  + publisherTextBox.Text
+                                  + ";"
+                                  + editionComboBox.Text;
+
+                //create an array of the size of the number of lines in the file
+                string[] readArray = new string[File.ReadLines(Globals.File).Count()];
+
+                StreamReader read2 = new StreamReader(Globals.File);
+                for (int i = 0; i < readArray.Length; i++)
+                {
+                    readArray[i] = read2.ReadLine();
+                }
+
+                read2.Close();
+
+                if (!readArray.Contains(fullLine)) // looking for a string in the array, 
+                                                    //if there is not one I write to the file
+                {
+
+                    using (StreamWriter flow2 = File.AppendText(Globals.File))
+                        flow2.WriteLine(fullLine);
+
+                    string[] arrayShow = new string[5] //line to display in the form
+                    {
+                        authorTextBox.Text,
+                        bookTextBox.Text,
+                        yearTextBox.Text,
+                        publisherTextBox.Text,
+                        editionComboBox.Text
+                    };
+                    ListViewItem lvi = new ListViewItem(arrayShow);
+                    listView1.Items.Add(lvi);
+
+                    //authorTextBox.Clear(); a code that can clear all fields filled in by the user in the form
+                    //bookTextBox.Clear();      
+                    //yearTextBox.Clear();
+                    //publisherTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Book already exists!"); // the line was in the array
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter author!"); // when empty input
+            }
+        }
+
+
+
+        private void buttonRec_Click(object sender, EventArgs e)
+        {
+            int lineCount = File.ReadLines(Globals.File).Count(); // count the lines in the file
+
+            if (lineCount != 0)
+            {
+                if (!String.IsNullOrEmpty(DelTextBox.Text)) // letter for deleting
+                {
+                    char deleteChar = DelTextBox.Text[0]; // crop if you enter more
+
+                    if (DelTextBox.Text.Length > 1)
+                    {
+                        MessageBox.Show(
+                            String.Format("You entered more then one letter! " +
+                                          "Filter will be applied to letter '{0}'", deleteChar));
+                    }
+
+                    string[] arrayDel = new string[lineCount]; // read an array for work
+
+                    StreamReader read = new StreamReader(Globals.File);
+                    for (int i = 0; i < lineCount; i++)
+                        arrayDel[i] = read.ReadLine();
+                    read.Close();
+
+                    //create a list of indexes of array strings beginning with a letter
+                    List<int> order = new List<int>();
+                    int orderString = 0;
+                    foreach (string j in arrayDel)
+                    {
+                        if (j.IndexOf(deleteChar) == 0)
+                        {
+                            order.Add(orderString); //fill out the list
+                        }
+
+                        orderString++;
+                    }
+
+                    if (order.Count == 0) // if the list is empty
+                    {
+                        MessageBox.Show("No authors found! Please choose another letter.");
+                        DelTextBox.Clear();
+                    }
+
+                    order.Reverse(); //reverse the list
+
+                    foreach (int j in order) // delete the rows in a combo list from the bottom of the array
+                    {
+                        arrayDel = arrayDel.Where(w => w != arrayDel[j]).ToArray(); // Linq
+                    }
+
+                    listView1.Items.Clear(); //clear the screen
+
+                    string[] split = new string[5]; //write a new array into a file and bring it to the screen
+                    StreamWriter flow = new StreamWriter(Globals.File);
+                    foreach (string j in arrayDel)
+                    {
+                        flow.WriteLine(j);
+                        split = j.Split(';');
+                        ListViewItem lvi = new ListViewItem(split);
+                        listView1.Items.Add(lvi);
+                    }
+
+                    flow.Close();
+                    DelTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please, choose a letter!"); //the letter was not entered
+                    DelTextBox.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Database is empty!");
+                DelTextBox.Clear();
+            }
+
+
+        }
+
+        private void DelTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar)) //Input protection. Only the letters
+
+            {
+                e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char) Keys.Back);
+            }
+        }
+
+        private void authorTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Input protection. Only the letters
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char) Keys.Back);
+        }
+
+        private void publisherTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Input protection. Only the letters
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char) Keys.Back);
+        }
+
+        private void yearTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Input protection. Only the numbers
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void publisherTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
